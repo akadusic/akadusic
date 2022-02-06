cb5ffa081c6c7b891308a95cc03e59ee4439d791
asmir98 <asmir.k17@gmail.com>
2022-02-06 23:33:16 +0100

    done

---
+++ Liste/Dvostruko_linkane_lista/build/CMakeFiles/mejn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mejn.dir/main.cpp.o"
+  "mejn"
+  "mejn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mejn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
